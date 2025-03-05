ee62658aefd23eadf2962333e09f6f34d48457c2
William Marlette <williammarlette3@gmail.com>
2025-03-05 17:49:39 -0500

    refractor rendering todo code into its own component file Todo.js

---
+++ components/Todo.js
@@ -0,0 +1,56 @@
+class Todo {
+  constructor(data, selector) {
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
+
+  _setEventListeners() {
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+    });
+
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  _generateCheckBoxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  _generateDueDate() {
+    const dueDate = new Date(this._data.date);
+    this._todoDate = this._todoElement.querySelector(".todo__date");
+    if (!isNaN(dueDate)) {
+      this._todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
+  }
+
+  getView() {
+    this._todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+    const todoNameEl = this._todoElement.querySelector(".todo__name");
+    const todoDate = this._todoElement.querySelector(".todo__date");
+    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    todoNameEl.textContent = this._data.name;
+
+    this._generateCheckBoxEl();
+    this._setEventListeners();
+    this._generateDueDate();
+
+    return this._todoElement;
+  }
+}
+
+export default Todo;
